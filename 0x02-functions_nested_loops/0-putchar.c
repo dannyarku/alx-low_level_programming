7c13e1cfe86b8221b5fee046be5d88f3116353ee
dannyarku <dannyarku@gmail.com>
2023-03-22 05:48:08 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * main - point putchar
+ * main - Write a program that prints Holberton, followed by a new*
  *
- * Return: Always 0 (success)
+ *
+ * Return: On success 0.
  */
 int main(void)
 {
-	_puttchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }
